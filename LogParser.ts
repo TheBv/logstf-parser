9ca794156f912d75247c3cde443a8e7c9e294171
c43721 <rlahman.game@gmail.com>
2021-08-02 19:54:25 -0500

    refactor: Format LogParser.ts

--- LogParser.ts
+++ LogParser.ts
@@ -1,37 +1,50 @@
 import * as events from './events'
-import {Game, IGameState} from './Game';
 import GameStateModule from "./modules/GameStateModule";
+import { Game, IGameState } from './Game';
 
 export class LogParser {
-    private modules : {new(gameState: IGameState): events.IStats }[]
-    private useCustom : boolean
-    constructor(){
+    private modules: {
+        new(gameState: IGameState): events.IStats 
+    }[]
+
+    private useCustom: boolean
+
+    constructor() {
         this.modules = []
         this.useCustom = false
     }
-    useCustomGameState(bool : boolean){
-        this.useCustom = true
+
+    useCustomGameState(){
+        this.useCustom = true 
     }
+    
     parseLines(lines: string[]): Game {
         const game = new Game()
+
         if (!this.useCustom){
             game.modules.push(new GameStateModule(game.gameState))
         }
+        
         for (const module of this.modules){
             game.modules.push(new module(game.gameState));
         }
+
         lines.forEach(line => game.processLine(line))
         game.finish()
+
         return game
     }
-    addModule(moduleClass: {new(gameState: IGameState): events.IStats}) {
+
+    addModule(moduleClass: { new (gameState: IGameState): events.IStats }) {
         this.modules.push(moduleClass);
     }
-    getModules(): {new(gameState: IGameState): events.IStats }[]{
+
+    getModules(): { new (gameState: IGameState): events.IStats }[]{
         return this.modules;
     }
 }
+
 //Export interfaces 
 export * as events from './events';
-export {IGameState,PlayerInfo} from './Game';
 export * from './modules/ModuleHolder';
+export { IGameState, PlayerInfo } from './Game';
